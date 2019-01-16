d6c34daefad097cf02e3cccdbd1e330baa7ae1a1
Billy Bunn <williammbunn@gmail.com>
2019-01-15 18:48:10 -0800

    add table render prototype

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-//HELPER FUNCTIONS
+// HELPER FUNCTIONS
 function calcRandomCustomersHourly(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
 }
@@ -16,78 +14,155 @@ function sumArray(arrName) { // Sums an array of numbers
   return arrName.reduce(sum);
 }
 
-/*
-NOTE:
-  Instead of doing the following to select individual element nodes:
-    var ulElement = document.getElementById('store-zero');
-  I used .querySelectorAll(), which selects multiple elements (a nodelist) by a CSS selector.
-  Page 188 in the Duckett Javascript book explains some of these terms and DOM query methods.
-  The rest of the expression -- Array.prototype.slice.call() -- stores the nodelist into an JS array I can loop through and use;
-  just like the ulElement variable above. There's some great explainations at the links below.
-https://www.w3schools.com/jsref/met_document_queryselectorall.asp
-https://davidwalsh.name/nodelist-array
-https://stackoverflow.com/questions/2125714/explanation-of-slice-call-in-javascript
-*/
 var ulElementsArray = Array.prototype.slice.call(document.querySelectorAll('.store'));
 var ulElementsArrayPosition = 0; // Starting position, moves forward one at each .render()
 // Important that each .render() is called in order
 
-var allLocations = [];
+// FOR TABLE RENDERING
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// Location Object Constructor Function
+var allLocations = [];
+var locationTable = document.getElementById('locations');
+
+
+// LOCATION OBJECT CONSTRUCTOR FUNCTION
 function Location(name, minCustomersEachHour, maxCustomersEachHour, avgCookiesPerCustomer) {
-  // Object properties
   this.name = name,
   this.minCustomersEachHour = minCustomersEachHour;
   this.maxCustomersEachHour = maxCustomersEachHour;
   this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+  this.totalDailyCookies = 0;
   this.arrCustomersEachHour = [];
   this.arrCookiesEachHour = [];
-  this.totalDailyCookies = 0;
-
-  // Object methods
-  this.calcCustomersEachHour = function () {
-    for (var i = 0; i < hours.length; i++) {
-      this.arrCustomersEachHour.push(calcRandomCustomersHourly(this.minCustomersEachHour, this.maxCustomersEachHour));
-    }
-  };
-  this.calcCookiesEachHour = function () {
-    for (var i = 0; i < hours.length; i++) {
-      this.arrCookiesEachHour.push(calcAvgCookiesHourly(this.avgCookiesPerCustomer, this.arrCustomersEachHour[i]));
-    }
-  };
-
-  this.render = function () {
-    this.calcCustomersEachHour(); //Calc random customers for each hour, store in array 'arrCustomersEachHour'
-    this.calcCookiesEachHour(); //Calc average cookies purchased each hour, store in array 'arrCookiesEachHour'
-    this.totalDailyCookies = sumArray(this.arrCookiesEachHour); //Calc total for the day and store in totalDailyCookies
-    for (var i = 0; i < hours.length; i++) { //Loop to display '#AM/PM: ## cookies' in a list
-      // 1. Create element to hold the data
-      var liEl = document.createElement('li');
-      // 2. Assign the data to the element
-      liEl.textContent = `${hours[i]}: ${this.arrCookiesEachHour[i]} cookies`;
-      // 3. Put the element into the DOM
-      ulElementsArray[ulElementsArrayPosition].appendChild(liEl);
-    }
-    //Adds the total as a final list item, 'Total: ## cookies'
-    var liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.totalDailyCookies} cookies`;
-    ulElementsArray[ulElementsArrayPosition].appendChild(liEl);
-    ulElementsArrayPosition++; //Increments for next Location instance
-    allLocations.push(this); //Pushes instance into array for console.table() below
-  };
+  for (var i = 0; i < hours.length; i++) {
+    this.arrCustomersEachHour.push(calcRandomCustomersHourly(this.minCustomersEachHour, this.maxCustomersEachHour));
+    this.arrCookiesEachHour.push(calcAvgCookiesHourly(this.avgCookiesPerCustomer, this.arrCustomersEachHour[i]));
+    this.totalDailyCookies = sumArray(this.arrCookiesEachHour);
+  }
+  allLocations.push(this);
 }
-
-var locationOne = new Location('1st and Pike', 23, 65, 6.3);
-var locationTwo = new Location('SeaTac Airport', 3, 24, 1.2);
-var locationThree = new Location('Seattle Center', 11, 38, 3.7);
-var locationFour = new Location('Capitol Hill', 20, 38, 2.3);
-var locationFive = new Location('Alki', 2, 16, 4.6);
-
-locationOne.render();
-locationTwo.render();
-locationThree.render();
-locationFour.render();
-locationFive.render();
+new Location('1st and Pike', 23, 65, 6.3);
+new Location('SeaTac Airport', 3, 24, 1.2);
+new Location('Seattle Center', 11, 38, 3.7);
+new Location('Capitol Hill', 20, 38, 2.3);
+new Location('Alki', 2, 16, 4.6);
 
 console.table(allLocations);
+
+
+
+// RENDER TABLE DATA PROTOTYPE
+Location.prototype.renderTable = function () {
+  // for (var i = 0; i < hours.length; i++) {
+  var trEl = document.createElement('tr'); // Make a <tr> (table row)
+
+  var tdEl = document.createElement('td'); // Make a <td> (table cell)
+  console.log('created td:', tdEl);
+  tdEl.textContent = this.name; // Add content to <td>
+  console.log('content created at', tdEl);
+  trEl.appendChild(tdEl); // Append for 'name'
+
+  for (var i = 0; i < hours.length; i++) {
+    // var trEl = document.createElement('tr');
+    var tdEl = document.createElement('td'); // Make a <td> (table cell)
+    tdEl.textContent = this.arrCookiesEachHour[i]; // Add content to <td>
+    trEl.appendChild(tdEl);
+  }
+
+  locationTable.appendChild(trEl);
+
+};
+
+
+// Separate function to make the table header
+function makeHeaderRow() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  for (var i = -1; i < hours.length; i++) { // 'i - 1' to create blank header cell above names
+    var thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Location Total';
+  trEl.appendChild(thEl);
+  locationTable.prepend(trEl);
+}
+
+// Function to render all locations
+function renderAllTables() {
+  for (var i = 0; i < allLocations.length; i++) {
+    allLocations[i].renderTable();
+  }
+}
+console.log(allLocations[0]);
+makeHeaderRow();
+renderAllTables();
+
+
+
+
+
+
+
+
+// locationOne.renderTable();
+
+
+
+
+// var locationOne = new Location('1st and Pike', 23, 65, 6.3);
+// var locationOne = new Location('1st and Pike', 23, 65, 6.3);
+// var locationTwo = new Location('SeaTac Airport', 3, 24, 1.2);
+// var locationThree = new Location('Seattle Center', 11, 38, 3.7);
+// var locationFour = new Location('Capitol Hill', 20, 38, 2.3);
+// var locationFive = new Location('Alki', 2, 16, 4.6);
+
+
+
+
+
+
+// locationTwo.render();
+// locationThree.render();
+// locationFour.render();
+// locationFive.render();
+// var locationOne = new Location('1st and Pike', 23, 65, 6.3);
+
+// makeHeaderRow();
+
+// locationOne.renderTableAll();
+
+
+
+
+
+// // Prototype method to render sales data in a list
+// Location.prototype.renderList = function () {
+//   this.calcCustomersEachHour(); //Calc random customers for each hour, store in array 'arrCustomersEachHour'
+//   this.calcCookiesEachHour(); //Calc average cookies purchased each hour, store in array 'arrCookiesEachHour'
+//   this.totalDailyCookies = sumArray(this.arrCookiesEachHour); //Calc total for the day and store in totalDailyCookies
+//   for (var i = 0; i < hours.length; i++) { //Loop to display '#AM/PM: ## cookies' in a list
+//     // 1. Create element to hold the data
+//     var liEl = document.createElement('li');
+//     // 2. Assign the data to the element
+//     liEl.textContent = `${hours[i]}: ${this.arrCookiesEachHour[i]} cookies`;
+//     // 3. Put the element into the DOM
+//     ulElementsArray[ulElementsArrayPosition].appendChild(liEl);
+//   }
+//   //Adds the total as a final list item, 'Total: ## cookies'
+//   var liEl = document.createElement('li');
+//   liEl.textContent = `Total: ${this.totalDailyCookies} cookies`;
+//   ulElementsArray[ulElementsArrayPosition].appendChild(liEl);
+
+//   ulElementsArrayPosition++; //Increments for next Location instance
+//   // allLocations.push(this); //Pushes instance into array for console.table()
+// };
+
+
+
+// function renderListAll() {
+//   for (var i = 0; i < ulElementsArray.length; i++) {
+//     allLocations[i].renderList();
+//   }
+// }
