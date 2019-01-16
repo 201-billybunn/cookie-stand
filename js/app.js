44e2a2116b55bbccd5d6be1fff746d4629a5b37c
Billy Bunn <williammbunn@gmail.com>
2019-01-16 08:55:39 -0800

    clean up. final for lab 07 assigment

--- js/app.js
+++ js/app.js
@@ -14,16 +14,11 @@ function sumArray(arrName) { // Sums an array of numbers
   return arrName.reduce(sum);
 }
 
-var ulElementsArray = Array.prototype.slice.call(document.querySelectorAll('.store'));
-var ulElementsArrayPosition = 0; // Starting position, moves forward one at each .render()
-// Important that each .render() is called in order
-
-// FOR TABLE RENDERING
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
 
 var allLocations = [];
+var grandTotal = [];
 var locationTable = document.getElementById('locations');
-
 
 // LOCATION OBJECT CONSTRUCTOR FUNCTION
 function Location(name, minCustomersEachHour, maxCustomersEachHour, avgCookiesPerCustomer) {
@@ -41,38 +36,30 @@ function Location(name, minCustomersEachHour, maxCustomersEachHour, avgCookiesPe
   }
   allLocations.push(this);
 }
-new Location('1st and Pike', 23, 65, 6.3);
-new Location('SeaTac Airport', 3, 24, 1.2);
-new Location('Seattle Center', 11, 38, 3.7);
-new Location('Capitol Hill', 20, 38, 2.3);
-new Location('Alki', 2, 16, 4.6);
-
-console.table(allLocations);
-
-
 
 // RENDER TABLE DATA PROTOTYPE
 Location.prototype.renderTable = function () {
-  // for (var i = 0; i < hours.length; i++) {
   var trEl = document.createElement('tr'); // Make a <tr> (table row)
 
+  // Loop to create, content, append for 'Name'
   var tdEl = document.createElement('td'); // Make a <td> (table cell)
-  console.log('created td:', tdEl);
   tdEl.textContent = this.name; // Add content to <td>
-  console.log('content created at', tdEl);
   trEl.appendChild(tdEl); // Append for 'name'
 
+  // Loop to create, content, append for 'arrCookiesEachHour'
   for (var i = 0; i < hours.length; i++) {
-    // var trEl = document.createElement('tr');
-    var tdEl = document.createElement('td'); // Make a <td> (table cell)
-    tdEl.textContent = this.arrCookiesEachHour[i]; // Add content to <td>
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.arrCookiesEachHour[i];
     trEl.appendChild(tdEl);
   }
 
+  // Create, content, append for 'Daily Total'
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.totalDailyCookies;
+  trEl.appendChild(tdEl);
+
   locationTable.appendChild(trEl);
-
 };
-
 
 // Separate function to make the table header
 function makeHeaderRow() {
@@ -95,74 +82,14 @@ function renderAllTables() {
     allLocations[i].renderTable();
   }
 }
-console.log(allLocations[0]);
+
+// Create Location instances
+new Location('1st and Pike', 23, 65, 6.3);
+new Location('SeaTac Airport', 3, 24, 1.2);
+new Location('Seattle Center', 11, 38, 3.7);
+new Location('Capitol Hill', 20, 38, 2.3);
+new Location('Alki', 2, 16, 4.6);
+
+console.table(allLocations);
 makeHeaderRow();
 renderAllTables();
-
-
-
-
-
-
-
-
-// locationOne.renderTable();
-
-
-
-
-// var locationOne = new Location('1st and Pike', 23, 65, 6.3);
-// var locationOne = new Location('1st and Pike', 23, 65, 6.3);
-// var locationTwo = new Location('SeaTac Airport', 3, 24, 1.2);
-// var locationThree = new Location('Seattle Center', 11, 38, 3.7);
-// var locationFour = new Location('Capitol Hill', 20, 38, 2.3);
-// var locationFive = new Location('Alki', 2, 16, 4.6);
-
-
-
-
-
-
-// locationTwo.render();
-// locationThree.render();
-// locationFour.render();
-// locationFive.render();
-// var locationOne = new Location('1st and Pike', 23, 65, 6.3);
-
-// makeHeaderRow();
-
-// locationOne.renderTableAll();
-
-
-
-
-
-// // Prototype method to render sales data in a list
-// Location.prototype.renderList = function () {
-//   this.calcCustomersEachHour(); //Calc random customers for each hour, store in array 'arrCustomersEachHour'
-//   this.calcCookiesEachHour(); //Calc average cookies purchased each hour, store in array 'arrCookiesEachHour'
-//   this.totalDailyCookies = sumArray(this.arrCookiesEachHour); //Calc total for the day and store in totalDailyCookies
-//   for (var i = 0; i < hours.length; i++) { //Loop to display '#AM/PM: ## cookies' in a list
-//     // 1. Create element to hold the data
-//     var liEl = document.createElement('li');
-//     // 2. Assign the data to the element
-//     liEl.textContent = `${hours[i]}: ${this.arrCookiesEachHour[i]} cookies`;
-//     // 3. Put the element into the DOM
-//     ulElementsArray[ulElementsArrayPosition].appendChild(liEl);
-//   }
-//   //Adds the total as a final list item, 'Total: ## cookies'
-//   var liEl = document.createElement('li');
-//   liEl.textContent = `Total: ${this.totalDailyCookies} cookies`;
-//   ulElementsArray[ulElementsArrayPosition].appendChild(liEl);
-
-//   ulElementsArrayPosition++; //Increments for next Location instance
-//   // allLocations.push(this); //Pushes instance into array for console.table()
-// };
-
-
-
-// function renderListAll() {
-//   for (var i = 0; i < ulElementsArray.length; i++) {
-//     allLocations[i].renderList();
-//   }
-// }
